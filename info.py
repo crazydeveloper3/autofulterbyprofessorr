8ce630bf8221412c8207b66ad350af496c1c6038
crazydeveloper3 <139277159+crazydeveloper3@users.noreply.github.com>
2024-03-31 20:21:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,8 +107,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://lazypro-2d3da1637738.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://lazypro-2d3da1637738.herokuapp.com/".format(FQDN, PORT)
+URL = "https://lazypro-46ed2d042752.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://lazypro-46ed2d042752.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -123,9 +123,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://lazypro-2d3da1637738.herokuapp.com/".format(FQDN)
+    URL = "https://lazypro-46ed2d042752.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://lazypro-2d3da1637738.herokuapp.com/".format(FQDN)
+    URL = "https://lazypro-46ed2d042752.herokuapp.com/".format(FQDN)
 
 
 
